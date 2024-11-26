031e7176d2e8cb3cdc56f0f526fded5d49adf13b
Nguyen Nguyen <nguyen@reverb.com>
2024-11-26 17:14:21 -0600

    wip

--- name/name.go
+++ name/name.go
@@ -1,13 +1,35 @@
 package name
 
 import (
+	"io"
+	"os"
+	"strings"
+  "crypto/md5"
+
 	"github.com/reverbdotcom/sbx/cli"
 )
 
 func Run() (string, error) {
-  return currentBranch()
+  return name()
 }
 
+func name() (string, error) {
+  branch, err := branch()
+
+  if err != nil {
+    return "", err
+  }
+
+  name, err := hash(branch)
+
+  if err != nil {
+    return "", err
+  }
+
+  return prefix(name), nil
+}
+
+var branch = currentBranch // need to mock for testing
 func currentBranch() (string, error) {
   out, err := cli.Cmd("git", "branch", "--show-current")
 
@@ -16,4 +38,47 @@ func currentBranch() (string, error) {
   }
 
   return out, nil
+}
+
+func hash(name string) (string, error) {
+  md5h := md5.Sum([]byte(name))
+  size := len(name)
+  words, err := properNames()
+
+  if err != nil {
+    return "", err
+  }
+
+  idx1 := int(md5h[0]) % size
+  idx2 := int(md5h[1]) % size
+  idx3 := int(md5h[2]) % size
+
+  names := []string{
+    words[idx1],
+    words[idx2],
+    words[idx3],
+  }
+
+  newName := strings.Join(names, "-")
+
+  return strings.ToLower(newName), nil
+}
+
+func prefix(name string) (string) {
+  return "sandbox-" + name
+}
+
+func properNames() ([]string, error) {
+	file, err := os.Open("/usr/share/dict/propernames")
+	if err != nil {
+    return []string{}, err
+	}
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+    return []string{}, err
+	}
+
+  // TODO only return > 3 characters
+	return strings.Split(string(bytes), "\n"), nil
 }
--- name/name_test.go
+++ name/name_test.go
@@ -5,4 +5,20 @@ import (
 )
 
 func TestName(t *testing.T) {
+  t.Run("it generates a sandbox name", func(t *testing.T) {
+    branch = func() (string, error) {
+      return "nn-sbx-123", nil
+    }
+
+    got, err := Run()
+    want := "sandbox-foo-bar-baz"
+
+    if err != nil {
+      t.Errorf("got %v, want nil", err)
+    }
+
+    if got != want {
+      t.Errorf("got %v, want %v", got, want)
+    }
+  })
 }
