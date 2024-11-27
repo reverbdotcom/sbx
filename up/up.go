76f337a193995a971b36f7a4f05958784b92bcff
Nguyen Nguyen <nguyen@reverb.com>
2024-11-26 19:06:33 -0600

    wip up command

--- up/up.go
+++ up/up.go
@@ -1,9 +1,47 @@
 package up
 
 import (
+  "strings"
+
 	"github.com/reverbdotcom/sbx/cli"
+	"github.com/reverbdotcom/sbx/name"
 )
 
+const noChanges = "up-to-date"
+
 func Run() (string, error) {
-	return cli.Cmd("git", "status")
+  name, err := name.Name()
+
+  if err != nil {
+    return "", err
+  }
+
+  branchOut, branchErr := cli.Cmd(
+    "git",
+    "branch",
+    "-f",
+    name,
+    "HEAD",
+  )
+
+  if branchErr != nil {
+    return branchOut, branchErr
+  }
+
+  out, err := cli.Cmd(
+    "git",
+    "push",
+    "origin",
+    name,
+  )
+
+  if err != nil {
+    return out, err
+  }
+
+  if strings.Contains(out, noChanges) {
+    return "sandbox branch up to date, make a new commit", nil
+  }
+
+  return out, nil
 }
