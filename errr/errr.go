0cc2606ebd6021e78ea4be13a4951139fb8d2bfa
Nguyen Nguyen <nguyen@reverb.com>
2024-11-27 15:13:51 -0600

    fix errr on parse

--- errr/errr.go
+++ errr/errr.go
@@ -3,14 +3,8 @@ package errr
 import (
 	"errors"
 	"fmt"
-
-	"github.com/reverbdotcom/sbx/commands"
 )
 
 func New(message string) error {
 	return errors.New(fmt.Sprintf("🚫 %s", message))
-}
-
-func NewWithHelp(message string) error {
-	return errors.New(fmt.Sprintf("🚫 %s\n\n\n%s", message, commands.Help))
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,8 +1,10 @@
 package parser
 
 import (
+  "fmt"
+  "errors"
+
 	"github.com/reverbdotcom/sbx/commands"
-	"github.com/reverbdotcom/sbx/errr"
 )
 
 func Parse(args []string) (*commands.RunFn, error) {
@@ -17,7 +19,7 @@ func Parse(args []string) (*commands.RunFn, error) {
 
 func command(args []string) (command *string, err error) {
 	if len(args) < 2 {
-		return nil, errr.NewWithHelp("command required")
+		return nil, errr("command required")
 	}
 
 	cmd := args[1]
@@ -29,8 +31,13 @@ func cmdfn(command string) (*commands.RunFn, error) {
 	cmd, ok := commands.Commands()[command]
 
 	if !ok {
-		return nil, errr.NewWithHelp("command not found")
+		return nil, errr("command not found")
 	}
 
 	return &cmd, nil
+}
+
+
+func errr(message string) error {
+	return errors.New(fmt.Sprintf("%s\n\n\n%s", message, commands.Help))
 }
