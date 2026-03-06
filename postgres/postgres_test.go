5cb26c3678b3108b865fec0af2787382f60e8bd5
naterod-reverb <nathaniel.rodriguez@reverb.com>
2026-03-06 16:11:29 -0600

    Remove unused OpenProgress() methods

--- postgres/postgres_test.go
+++ postgres/postgres_test.go
@@ -40,23 +40,3 @@ func TestOpen_Error(t *testing.T) {
 		t.Error("Expected error, got nil")
 	}
 }
-
-func TestOpenProgress(t *testing.T) {
-	htmlUrlFn = func() (string, error) {
-		return "https://github.com/test", nil
-	}
-
-	openURL = func(url string) error {
-		expectedUrl := "https://github.com/test"
-		if url != expectedUrl {
-			t.Errorf("Expected url %s, got %s", expectedUrl, url)
-		}
-		return nil
-	}
-
-	_, err := OpenProgress()
-
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-}
